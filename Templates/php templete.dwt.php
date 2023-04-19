7aef190e6c54d1064aa0d35d8f3790f08f5a34d6
Madan Devkota <88671618+DevkotaMadan@users.noreply.github.com>
2023-04-19 12:46:53 -0700

    Initial commit

---
+++ Templates/php templete.dwt.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title><!-- TemplateBeginEditable name="titile" -->Title name goes here<!-- TemplateEndEditable --></title>
+
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+	<?php include("../includes/header.php"); ?>
+	<?php include("../includes/nav.php"); ?>
+	<main>
+	<!-- TemplateBeginEditable name="main" -->Content for New main Tag Goes Here<!-- TemplateEndEditable -->
+	</main>
+	<?php include("../includes/footer.php"); ?>
+	<script src="../js/main.js"></script>
+
+</body>
+</html>
---
+++ shop.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/php templete.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+
+<title><!-- InstanceBeginEditable name="titile" -->Title name goes here<!-- InstanceEndEditable --></title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+	<?php include("includes/header.php"); ?>
+	<?php include("includes/nav.php"); ?>
+	<main>
+	<!-- InstanceBeginEditable name="main" -->Content for New main Tag Goes Here<!-- InstanceEndEditable -->
+	</main>
+	<?php include("includes/footer.php"); ?>
+	<script src="js/main.js"></script>
+
+</body>
+<!-- InstanceEnd --></html>
